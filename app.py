e7253561dcde286c7e0c2ca3342b7f1f02b819a3
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-08-23 00:30:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from supabase import create_client
 from streamlit_autorefresh import st_autorefresh
 
 # CONFIG --------------------------------------------------------------------------------------------------------------------------------------
-st.set_page_config(page_title="مين أكثر؟", page_icon="🎉", ayout="wide")
+st.set_page_config(page_title="مين أكثر؟", page_icon="🎉", layout="wide")
 VOTING_SECONDS = 10
 
 # SUPABASE --------------------------------------------------------------------------------------------------------------------------------------
